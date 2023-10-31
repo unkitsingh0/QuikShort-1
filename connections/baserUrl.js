97f78693ea66a03d8c57d3d17304fa94edb1fb5e
Ankit Singh <52825138+unkitsingh0@users.noreply.github.com>
2023-10-31 10:02:29 +0530

    updating base url of backend host

--- connections/baserUrl.js
+++ connections/baserUrl.js
@@ -1,2 +1,3 @@
-const baseUrl = "http://localhost:8032";
+// const baseUrl = "http://localhost:8032"; // // This is for development in local system
+const baseUrl = "https://qs-pibk.onrender.com";
 export default baseUrl;
